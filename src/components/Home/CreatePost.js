0c80714714b521d586766ce29d4b0e832c013876
ValeedAnjum <allinone644@gmail.com>
2020-06-22 17:18:11 +0500

    storing post data to firestore

--- src/components/Home/CreatePost.js
+++ src/components/Home/CreatePost.js
@@ -1,19 +1,21 @@
-import React , { useState, useEffect } from 'react'
+import React , { useState } from 'react'
 import Resizer from 'react-image-file-resizer';
 import { connect } from 'react-redux';
 import {addPost} from '../../store/Actions/UserActions';
+<<<<<<< HEAD
 import { fetchPost } from '../../store/Actions/PostActions';
 const CreatePost = ({ photoUrl, addPost, uploading, uploadingPercentage }) => {
     const [image, setimage] = useState(null);
     const [video, setvideo] = useState(null);
+=======
+const CreatePost = ({ photoUrl, addPost }) => {
+    const [file, setfile] = useState(null);
+>>>>>>> parent of c49ba83... video and images uploading to firebase storage
     const [userStory, setuserStory] = useState("");
     const imageClickHandler = () => {
         document.getElementById('user-post-image').click();
     }
-    const videoClickHandler = () => {
-        document.getElementById('user-post-video').click();
-    }
-    const imageChangedHandler  = event => {
+    const fileChangedHandler  = event => {
         var fileInput = false
         if(event.target.files[0]) {
             fileInput = true
@@ -27,8 +29,12 @@ const CreatePost = ({ photoUrl, addPost, uploading, uploadingPercentage }) => {
                 100,
                 0,
                 uri => {
+<<<<<<< HEAD
                     setimage(uri);
                     setvideo(null);
+=======
+                    setfile(uri);
+>>>>>>> parent of c49ba83... video and images uploading to firebase storage
                     if(uri){
                         var demoImage = document.getElementById('display-user-post-image');
                         demoImage.src = uri;
@@ -38,6 +44,7 @@ const CreatePost = ({ photoUrl, addPost, uploading, uploadingPercentage }) => {
             );
         }
     }
+<<<<<<< HEAD
     const videoChnageHandler = event => {
         // console.log(event.target.files[0]);
         const file = event.target.files[0];
@@ -62,16 +69,15 @@ const CreatePost = ({ photoUrl, addPost, uploading, uploadingPercentage }) => {
         }else if(userStory.trim().length > 0){
             addPost(null,userStory.trim());
         }
+=======
+    const savePost = async () => {
+        await addPost(file,userStory);
+        console.log('back');
+>>>>>>> parent of c49ba83... video and images uploading to firebase storage
     }
     const handleUserStory = event => {
         setuserStory(event.target.value);
     }
-    useEffect(() => {
-        if(!uploading){
-            setimage(null);
-            setuserStory("");
-        }
-    },[uploading])
     return (
         <div className="create-post">
             <h4>Create Post</h4>
@@ -87,30 +93,35 @@ const CreatePost = ({ photoUrl, addPost, uploading, uploadingPercentage }) => {
             </div>
             <div className="upload-content">
                 <div className="upload-picture">
+<<<<<<< HEAD
                     <input type="file" id="user-post-image" accept="image/*" onChange={imageChangedHandler} />
                     <input type="file" id="user-post-video" accept="video/*" onChange={videoChnageHandler} />
+=======
+                    <input type="file" id="user-post-image" onChange={fileChangedHandler} />
+>>>>>>> parent of c49ba83... video and images uploading to firebase storage
                     <button onClick={imageClickHandler}>Photo</button>
                 </div>
                 <div className="upload-video">
                     <input type="file" />
-                    <button onClick={videoClickHandler}>Video</button>
+                    <button>Video</button>
                 </div>
             </div>
             <div className="user-post-image">
                 {
-                    image ? <img src={image} id="display-user-post-image" />:null
-                }
-                {
-                    video ? <video src={video} controls />:null
+                    file ? <img src={file} id="display-user-post-image" />:null
                 }
             </div>
             <div className="post-button-container">
+<<<<<<< HEAD
                 {
                     !uploading ? <button
                     onClick={savePost}>Post</button>:<button>Posting...({
                         uploadingPercentage ? uploadingPercentage.toFixed(2):null}%)</button>
                 }
                 
+=======
+                <button onClick={savePost}>Post</button>
+>>>>>>> parent of c49ba83... video and images uploading to firebase storage
             </div>
         </div>
     )
@@ -118,8 +129,12 @@ const CreatePost = ({ photoUrl, addPost, uploading, uploadingPercentage }) => {
 
 const mapState = state => {
     return {
+<<<<<<< HEAD
         uploading:state.User.uploading,
         uploadingPercentage:state.User.uploadingPercentage        
+=======
+
+>>>>>>> parent of c49ba83... video and images uploading to firebase storage
     }
 }
 const mapDispatch = dispatch => {
