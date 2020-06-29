f79e7e50a1343a5fcc211b67b2d9cf3733d5e832
ValeedAnjum <allinone644@gmail.com>
2020-06-29 09:52:53 +0500

    working on comment replies

---
+++ src/components/Home/CommentsReplies.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CommentsReplies = ({commentId}) => {
+    console.log(commentId);
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CommentsReplies
--- src/components/Home/SingleComment.js
+++ src/components/Home/SingleComment.js
@@ -2,7 +2,6 @@ import React, { Fragment } from 'react'
 
 const SingleComment = ({comment}) => {
     const {likes, message, name, profileimage, replies} = comment;
-    console.log(replies.length);
     return (
         <div className="single-comment">
             <img src="style/images/user.jpg" alt="comment"/>
@@ -17,16 +16,17 @@ const SingleComment = ({comment}) => {
                     <span>29m</span>
                 </div>
             </div>
-            {(replies.length >= 1)
+            {(replies >= 1)
                 ? <Fragment>
                         <div className="comment-replies">
                             <i className="fas fa-arrow-down"></i>
                             <h6>
-                                4 Replies</h6>
+                                {replies} Replies</h6>
                         </div>
                     </Fragment>
                 : null
-}
+            }
+
         </div>
     )
 }
